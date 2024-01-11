6314e160ec4f1a1bada1f7e837a9104ab78589b9
Alan <alan360gabriel@gmail.com>
2024-01-11 18:28:27 -0300

    Criação da entidade 'Product' e seu repositório usando typeorm

---
+++ src/modules/products/typeorm/repositories/ProductsRepository.ts
@@ -0,0 +1,14 @@
+import Product from '../entities/Product';
+import { appDataSource } from '@shared/typeorm/index';
+
+const ProductRepository = appDataSource.getRepository(Product).extend({
+   async findByName(name: string): Promise<Product | null> {
+      const product = this.findOne({
+         where: {
+            name,
+         },
+      });
+
+      return product;
+   },
+});
