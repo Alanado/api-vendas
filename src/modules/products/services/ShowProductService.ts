d778a79a4f48b6ced22472e68b2c29f2c96d36a0
Alan <alan360gabriel@gmail.com>
2024-01-11 19:24:49 -0300

    Implementação do service de mostrar um produto/ modificação do service.

--- src/modules/products/services/ShowProductService.ts
+++ src/modules/products/services/ShowProductService.ts
@@ -1,9 +1,14 @@
+import AppError from '@shared/errors/AppError';
 import Product from '../typeorm/entities/Product';
 import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
 
 export default class ShowProductService {
    public async execute(id: string): Promise<Product | null> {
       const product = await ProductRepository.findOne({ where: { id } });
+
+      if (!product) {
+         throw new AppError('Produto não encontrado.', 404);
+      }
 
       return product;
    }
