36a2d71e62e9f0702e1def95418d5c548730fad2
Alan <alan360gabriel@gmail.com>
2024-01-11 19:45:34 -0300

    Implementação do service para excluir um produto.

---
+++ src/modules/products/services/DeleteProductService copy.ts
@@ -0,0 +1,14 @@
+import AppError from '@shared/errors/AppError';
+import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
+
+export default class DeleteProductService {
+   public async execute(id: string): Promise<void> {
+      const product = await ProductRepository.findOne({ where: { id } });
+
+      if (!product) {
+         throw new AppError('Produto não encontrado.', 404);
+      }
+
+      await ProductRepository.remove(product);
+   }
+}
