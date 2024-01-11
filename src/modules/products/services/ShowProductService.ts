26812a9e87785e168ac32483453549bf1e78b0ba
Alan <alan360gabriel@gmail.com>
2024-01-11 19:38:57 -0300

    Implementação do service para atualizar um produto.

--- src/modules/products/services/ShowProductService.ts
+++ src/modules/products/services/ShowProductService.ts
@@ -3,7 +3,7 @@ import Product from '../typeorm/entities/Product';
 import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
 
 export default class ShowProductService {
-   public async execute(id: string): Promise<Product | null> {
+   public async execute(id: string): Promise<Product> {
       const product = await ProductRepository.findOne({ where: { id } });
 
       if (!product) {
---
+++ src/modules/products/services/UpdateProductService.ts
@@ -0,0 +1,39 @@
+import AppError from '@shared/errors/AppError';
+import Product from '../typeorm/entities/Product';
+import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
+
+interface IRequest {
+   id: string;
+   name: string;
+   price: number;
+   quantity: number;
+}
+
+export default class UpdateProductService {
+   public async execute({
+      name,
+      price,
+      quantity,
+      id,
+   }: IRequest): Promise<Product> {
+      const product = await ProductRepository.findOne({ where: { id } });
+
+      if (!product) {
+         throw new AppError('Produto não encontrado.', 404);
+      }
+
+      const productExist = await ProductRepository.findByName(name);
+
+      if (productExist && name !== product.name) {
+         throw new AppError('Já existe um produto com este nome.');
+      }
+
+      product.name = name;
+      product.price = price;
+      product.quantity = quantity;
+
+      await ProductRepository.save(product);
+
+      return product;
+   }
+}
