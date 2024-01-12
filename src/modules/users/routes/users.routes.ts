0ee601f570e9b04fdf5449921ebb67644f429888
Alan <alan360gabriel@gmail.com>
2024-01-12 11:10:03 -0300

    Definido rotas de usuários/users.

---
+++ src/modules/users/routes/users.routes.ts
@@ -0,0 +1,34 @@
+import { Router } from 'express';
+import { celebrate, Joi, Segments } from 'celebrate';
+import UserController from '../controllers/UsersController';
+
+const userRoutes = Router();
+const userController = new UserController();
+
+userRoutes.get('/', userController.index);
+userRoutes.post(
+   '/',
+   celebrate({
+      [Segments.BODY]: {
+         name: Joi.string().required().messages({
+            'any.required': 'O nome de usuário é obrigatório.',
+            'string.empty': 'O nome de usuário é obrigatório.',
+            'string.base': 'Insira um nome de usuário válido.',
+         }),
+         email: Joi.string().email().required().messages({
+            'any.required': 'O email é obrigatório.',
+            'string.empty': 'O email é obrigatório.',
+            'string.base': 'Insira um email válido.',
+            'string.email': 'Insira um email válido. E',
+         }),
+         password: Joi.string().required().messages({
+            'any.required': 'A senha é obrigatório.',
+            'string.empty': 'A senha é obrigatório.',
+            'string.base': 'Insira uma senha válida.',
+         }),
+      },
+   }),
+   userController.create,
+);
+
+export default userRoutes;
--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import productRoutes from '@modules/products/routes/products.routes';
+import userRoutes from '@modules/users/routes/users.routes';
 
 const routes = Router();
 
 routes.use('/products', productRoutes);
+routes.use('/users', userRoutes);
 
 export default routes;
