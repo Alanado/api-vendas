95a78dcf4e429c8d6326eff5f6b1a1e5e896b271
Alan <alan360gabriel@gmail.com>
2024-01-17 21:24:19 -0300

    Adicionando configuração inicial do Ethereal, para fake-email

--- src/config/mail/EtherealMail.ts
+++ src/config/mail/EtherealMail.ts
@@ -1,4 +1,4 @@
-import nodemailer from "nodemailer"
+import nodemailer from 'nodemailer';
 
 interface ISendMail {
    to: string;
@@ -7,6 +7,30 @@ interface ISendMail {
 
 export default class EtherealMail {
    static async sendMail({ to, body }: ISendMail) {
-      const account =
+      const account = await nodemailer.createTestAccount();
+
+      const transporter = nodemailer.createTransport({
+         host: account.smtp.host,
+         port: account.smtp.port,
+         secure: account.smtp.secure,
+         auth: {
+            user: account.user,
+            pass: account.pass,
+         },
+      });
+
+      const message = await transporter.sendMail({
+         from: 'alanzokabrazuca@gmail.com',
+         to,
+         subject: 'Redefinição de senha.',
+         text: body,
+      });
+
+      console.log(`Mensagem enviada: ${message}`);
+      console.log(
+         `URL para ver caixa de entrada: ${nodemailer.getTestMessageUrl(
+            message,
+         )}`,
+      );
    }
 }
