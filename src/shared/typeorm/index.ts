e034c0a0a05dac73462d99bc8d0a267297d10c54
Alan Gabriel <alan360gabriel@gmail.com>
2024-01-19 09:43:28 -0300

    criação do modulo de customers, juntamente de sua entidade Customer e se repositório.

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -2,10 +2,12 @@ import { DataSource } from 'typeorm';
 import { CreateProducts1704932047720 } from '@shared/typeorm/migrations/1704932047720-CreateProducts';
 import { CreateUser1705062556276 } from '@shared/typeorm/migrations/1705062556276-createUser';
 import { CreateUserTokens1705520807828 } from './migrations/1705520807828-CreateUserTokens';
+import { CreateCustomers1705666680907 } from './migrations/1705666680907-CreateCustomers';
 
 import Product from '@modules/products/typeorm/entities/Product';
 import User from '@modules/users/typeorm/entities/User';
 import UserToken from '@modules/users/typeorm/entities/UserToken';
+import Customer from '@modules/customers/typeorm/entities/Customer';
 
 export const appDataSource = new DataSource({
    type: 'postgres',
@@ -14,10 +16,11 @@ export const appDataSource = new DataSource({
    username: 'postgres',
    password: 'postgres',
    database: 'api_vendas',
-   entities: [Product, User, UserToken],
+   entities: [Product, User, UserToken, Customer],
    migrations: [
       CreateProducts1704932047720,
       CreateUser1705062556276,
       CreateUserTokens1705520807828,
+      CreateCustomers1705666680907,
    ],
 });
