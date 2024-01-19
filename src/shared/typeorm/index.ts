2fd11128ceb8163e651eb19fba6d1163132a93fd
Alan Gabriel <alan360gabriel@gmail.com>
2024-01-19 19:47:37 -0300

    migration tabela orders_products

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -5,6 +5,9 @@ import { CreateUserTokens1705520807828 } from './migrations/1705520807828-Create
 import { CreateCustomers1705666680907 } from './migrations/1705666680907-CreateCustomers';
 import { CreateOrders1705701647467 } from './migrations/1705701647467-CreateOrders';
 import { AddCustomerIdToOrders1705702254717 } from './migrations/1705702254717-AddCustomerIdToOrders';
+import { CreateOrdersProducts1705703420307 } from './migrations/1705703420307-CreateOrdersProducts';
+import { AddProductIdToOrdersProducts1705703854107 } from './migrations/1705703854107-AddProductIdToOrdersProducts';
+import { AddOrderIdToOrdersProducts1705703656614 } from './migrations/1705703656614-AddOrderIdToOrdersProducts';
 
 import Product from '@modules/products/typeorm/entities/Product';
 import User from '@modules/users/typeorm/entities/User';
@@ -26,5 +29,8 @@ export const appDataSource = new DataSource({
       CreateCustomers1705666680907,
       CreateOrders1705701647467,
       AddCustomerIdToOrders1705702254717,
+      CreateOrdersProducts1705703420307,
+      AddProductIdToOrdersProducts1705703854107,
+      AddOrderIdToOrdersProducts1705703656614,
    ],
 });
