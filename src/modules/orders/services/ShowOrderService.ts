10ee43bb33dc7842779b6c8d4366812b8a70e496
Alan Gabriel <alan360gabriel@gmail.com>
2024-01-20 18:54:07 -0300

    criando service para listar um pedido/order

---
+++ src/modules/orders/services/ShowOrderService.ts
@@ -0,0 +1,19 @@
+import AppError from '@shared/errors/AppError';
+import Order from '../typeorm/entities/Order';
+import OrderRepository from '../typeorm/repositories/OrdersRepository';
+
+interface IRequest {
+   id: string;
+}
+
+export default class CreateUserService {
+   public async execute({ id }: IRequest): Promise<Order[]> {
+      const order = await OrderRepository.findById(id);
+
+      if (!order) {
+         throw new AppError('Pedido não encontrado.');
+      }
+
+      return order;
+   }
+}
